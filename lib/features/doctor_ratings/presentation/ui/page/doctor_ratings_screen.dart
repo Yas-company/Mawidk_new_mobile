2c432b9e84cd5949942bc2819c75470827943a70
omarzezo <zezoomar674@yahoo.com>
2025-05-27 13:14:40 +0300

    make_animation

--- lib/features/doctor_ratings/presentation/ui/page/doctor_ratings_screen.dart
+++ lib/features/doctor_ratings/presentation/ui/page/doctor_ratings_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -24,72 +26,124 @@ class DoctorRatingsScreen extends StatefulWidget {
   State<DoctorRatingsScreen> createState() => DoctorRatingsScreenState();
 }
 
-class DoctorRatingsScreenState extends State<DoctorRatingsScreen> {
+class DoctorRatingsScreenState extends State<DoctorRatingsScreen> with TickerProviderStateMixin {
   DoctorRatingsBloc doctorRatingsBloc = DoctorRatingsBloc(doctorRatingsUseCase:getIt());
+  List<bool> _visibleItems = [];
+
+  Future<void> refreshData() async {
+    final completer = Completer<void>();
+    // Listen once for the loaded state
+    final subscription = doctorRatingsBloc.stream.listen((state) {
+      if (state is LoadedState) {
+        final list = (state.data).model?.model ?? [];
+        _runStaggeredAnimation(list.length);
+        completer.complete();
+      }
+    });
+    // Trigger refresh
+    doctorRatingsBloc.add(ApplyDoctorRatingsEvent(id: widget.id));
+
+    // Wait for completion
+    await completer.future;
+    await subscription.cancel();
+  }
+
+
+  void _runStaggeredAnimation(int count) async {
+    _visibleItems = List.generate(count, (_) => false);
+    for (int i = 0; i < count; i++) {
+      await Future.delayed(const Duration(milliseconds: 100));
+      if (mounted) {
+        setState(() {
+          _visibleItems[i] = true;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MediaQuery.removePadding(
       context: context, removeTop: true,
-      child: Scaffold(
-        backgroundColor:AppColors.whiteBackground,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(100),
-          child: Padding(
-            padding: const EdgeInsets.only(top:24),
-            // child: appBar(context: context,backBtn: true,text:widget.name,isCenter:true,actions:[
-            child: appBar(context: context,backBtn: true,text:'all_evaluations'.tr(),isCenter:true,actions:[
-              // Container(margin:EdgeInsets.only(top:20),
-              //     padding:EdgeInsets.all(11),
-              //     decoration:BoxDecoration(shape:BoxShape.circle,
-              //         border: Border.all(color:AppColors.grey100)),
-              //     child:PImage(source:AppSvgIcons.share,width:20,height:18,)),
-              // const SizedBox(width:10,),
-              // Container(margin:EdgeInsets.only(top:20),
-              //     padding:EdgeInsets.all(11),
-              //     decoration:BoxDecoration(shape:BoxShape.circle,
-              //         border: Border.all(color:AppColors.grey100)),
-              //     child:PImage(source: AppSvgIcons.love,width:20,height:18)),
-              // const SizedBox(width:10,),
-            ]),
+      child: RefreshIndicator(
+        backgroundColor: AppColors.primaryColor,
+        color: AppColors.whiteColor,
+        onRefresh: refreshData,
+        child: Scaffold(
+          backgroundColor:AppColors.whiteBackground,
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(100),
+            child: Padding(
+              padding: const EdgeInsets.only(top:24),
+              // child: appBar(context: context,backBtn: true,text:widget.name,isCenter:true,actions:[
+              child: appBar(context: context,backBtn: true,text:'all_evaluations'.tr(),isCenter:true,actions:[
+                // Container(margin:EdgeInsets.only(top:20),
+                //     padding:EdgeInsets.all(11),
+                //     decoration:BoxDecoration(shape:BoxShape.circle,
+                //         border: Border.all(color:AppColors.grey100)),
+                //     child:PImage(source:AppSvgIcons.share,width:20,height:18,)),
+                // const SizedBox(width:10,),
+                // Container(margin:EdgeInsets.only(top:20),
+                //     padding:EdgeInsets.all(11),
+                //     decoration:BoxDecoration(shape:BoxShape.circle,
+                //         border: Border.all(color:AppColors.grey100)),
+                //     child:PImage(source: AppSvgIcons.love,width:20,height:18)),
+                // const SizedBox(width:10,),
+              ]),
+            ),
           ),
-        ),
-        body: BlocProvider(
-          create: (context) => doctorRatingsBloc,
-          child:Center(
-            child: PBlocBuilder<DoctorRatingsBloc,BaseState>(bloc:doctorRatingsBloc,
-                init:() {
-                  // doctorRatingsBloc.add(ApplyDoctorRatingsEvent(id: id));
-                  doctorRatingsBloc.add(ApplyDoctorRatingsEvent(id: widget.id));
-                },loadingWidget: Center(child:CustomLoader(size:35,)),
-                loadedWidget:(state) {
-                  List<DoctorRating> dataList = ((state as LoadedState).data).model?.model ?? [];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:20),
-                    child: Column(children: [
-                      Row(mainAxisAlignment:MainAxisAlignment.spaceBetween,children: [
-                        PText(title: 'evaluations'.tr(),fontWeight:FontWeight.w700,),
-                        Stack(children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 3), // Adjust this value for spacing
-                              child: PText(fontWeight:FontWeight.w500,size:PSize.text13,
-                                title:'${dataList.length}  تقييم',fontColor:AppColors.grey200,
+          body: BlocProvider(
+            create: (context) => doctorRatingsBloc,
+            child:Center(
+              child: PBlocBuilder<DoctorRatingsBloc,BaseState>(bloc:doctorRatingsBloc,
+                  init:() {
+                    // doctorRatingsBloc.add(ApplyDoctorRatingsEvent(id: id));
+                    doctorRatingsBloc.add(ApplyDoctorRatingsEvent(id: widget.id));
+                  },loadingWidget: Center(child:CustomLoader(size:35,)),
+                  loadedWidget:(state) {
+                    List<DoctorRating> dataList = ((state as LoadedState).data).model?.model ?? [];
+        
+                    if (_visibleItems.length != dataList.length) {
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        _runStaggeredAnimation(dataList.length);
+                      });
+                    }
+        
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:20),
+                      child: Column(children: [
+                        Row(mainAxisAlignment:MainAxisAlignment.spaceBetween,children: [
+                          PText(title: 'evaluations'.tr(),fontWeight:FontWeight.w700,),
+                          Stack(children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 3), // Adjust this value for spacing
+                                child: PText(fontWeight:FontWeight.w500,size:PSize.text13,
+                                  title:'${dataList.length}  تقييم',fontColor:AppColors.grey200,
+                                ),
                               ),
-                            ),
-                            Positioned(bottom:5, left: 0, right: 0,
-                              child: Container(height: 1,
-                                color:AppColors.grey200,),
-                            ),
-                          ],
-                        )
+                              Positioned(bottom:5, left: 0, right: 0,
+                                child: Container(height: 1,
+                                  color:AppColors.grey200,),
+                              ),
+                            ],
+                          )
+                        ],),
+                        ListView.builder(padding:EdgeInsets.only(top:20),
+                          itemBuilder:(context, index) {
+                          final visible = _visibleItems.length > index && _visibleItems[index];
+                          return AnimatedOpacity(
+                              opacity: visible ? 1.0 : 0.0,
+                              duration: const Duration(milliseconds: 500),
+                              child: AnimatedSlide(
+                              duration: const Duration(milliseconds: 500),
+                                  offset: visible ? Offset.zero : const Offset(0, 0.2),
+                                  child: DoctorRatingsItem(doctorRating:dataList[index])));
+                        },itemCount:dataList.length,shrinkWrap:true,)
                       ],),
-                      ListView.builder(padding:EdgeInsets.only(top:20),
-                        itemBuilder:(context, index) {
-                        return DoctorRatingsItem(doctorRating:dataList[index]);
-                      },itemCount:dataList.length,shrinkWrap:true,)
-                    ],),
-                  );
-                },),
-          )
+                    );
+                  },),
+            )
+          ),
         ),
       ),
     );
--- lib/features/patient_favourite/presentation/ui/page/patient_favourite_screen.dart
+++ lib/features/patient_favourite/presentation/ui/page/patient_favourite_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,60 +17,147 @@ import 'package:mawidak/features/patient_favourite/presentation/bloc/favourite_b
 import 'package:mawidak/features/patient_favourite/presentation/bloc/favourite_event.dart';
 import 'package:mawidak/features/patient_favourite/presentation/ui/widgets/favourite_item_widget.dart';
 
-class PatientFavouriteScreen extends StatelessWidget {
+class PatientFavouriteScreen extends StatefulWidget {
   const PatientFavouriteScreen({super.key});
+
+
+  @override
+  State<PatientFavouriteScreen> createState() => PatientFavouriteScreenState();
+}
+
+class PatientFavouriteScreenState extends State<PatientFavouriteScreen> with TickerProviderStateMixin {
+  FavouriteBloc favouriteBloc = FavouriteBloc(favouriteUseCase: getIt());
+  List<bool> _visibleItems = [];
+
+  Future<void> refreshData() async {
+    final completer = Completer<void>();
+    // Listen once for the loaded state
+    final subscription = favouriteBloc.stream.listen((state) {
+      if (state is LoadedState) {
+        final list = (state.data).model?.model ?? [];
+        _runStaggeredAnimation(list.length);
+        completer.complete();
+      }
+    });
+    // Trigger refresh
+    favouriteBloc.add(ApplyFavouriteEvent());
+
+    // Wait for completion
+    await completer.future;
+    await subscription.cancel();
+  }
+
+
+  void _runStaggeredAnimation(int count) async {
+    _visibleItems = List.generate(count, (_) => false);
+    for (int i = 0; i < count; i++) {
+      await Future.delayed(const Duration(milliseconds: 100));
+      if (mounted) {
+        setState(() {
+          _visibleItems[i] = true;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    FavouriteBloc favouriteBloc = FavouriteBloc(favouriteUseCase: getIt());
-    return BlocProvider(create: (context) => favouriteBloc,
-      child: Scaffold(backgroundColor: AppColors.whiteBackground,
-      body:SafeArea(
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.only(top:30,bottom:14),
-            child: PText(title: 'favourite'.tr()),
-          ),
-          PBlocBuilder<FavouriteBloc,BaseState>(bloc:favouriteBloc,
-              init:() {
-                favouriteBloc.add(ApplyFavouriteEvent());
-              },loadedWidget:(state) {
-              List<FavouriteDoctorListData> dataList = ((state as LoadedState).data).model?.model ?? [];
-                if (dataList.isEmpty) {
-                  return Expanded(child: PText(title: 'no_doctors_found'.tr()));
-                } else {
-                  return Expanded(
-                  child: ListView.builder(itemBuilder:(context, index) {
-                    FavouriteDoctorListData item = dataList[index];
-                    return FavouriteItemWidget(onCardClick:() {
-                      context.push(AppRouter.doctorProfileScreen,extra:{
-                        'id':item.doctor?.id??0,
-                        'name':item.doctor?.name??'',
-                        'specialization':item.doctor?.specialization??''
-                      });
-                    },onTap:() {
-                      context.push(AppRouter.appointmentBookingScreen,extra:{
-                        'id':item.doctor?.id??0,
-                        'name':item.doctor?.name??'',
-                        'specialization':item.doctor?.specialization??''
-                      });
-                    },imageUrl:item.doctor?.photo??'',
-                        doctorName:item.doctor?.name??'',
-                        rating:4, location:'', specialization:item.doctor?.specialization??'');
-                  },shrinkWrap: true,itemCount:dataList.length,),
-                );
-                }
-              },loadingWidget:Expanded(child: Center(child:CustomLoader(size:35,))),
-             emptyWidget:(state) {
-            return Expanded(child: Center(child: Column(mainAxisSize: MainAxisSize.min,
+    return BlocProvider(
+      create: (context) => favouriteBloc,
+      child: RefreshIndicator(
+        backgroundColor: AppColors.primaryColor,
+        color: AppColors.whiteColor,
+        onRefresh: refreshData,
+        child: Scaffold(
+          backgroundColor: AppColors.whiteBackground,
+          body: SafeArea(
+            child: Column(crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Icon(Icons.hourglass_empty),
-                PText(title: 'no_doctors_found'.tr(),size:PSize.text18,),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30, bottom: 14),
+                  child: PText(title: 'favourite'.tr()),
+                ),
+                PBlocBuilder<FavouriteBloc, BaseState>(
+                  bloc: favouriteBloc,
+                  init: () {
+                    favouriteBloc.add(ApplyFavouriteEvent());
+                  },
+                  loadedWidget: (state) {
+                    List<FavouriteDoctorListData> dataList =
+                        ((state as LoadedState).data).model?.model ?? [];
+
+                    if (_visibleItems.length != dataList.length) {
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        _runStaggeredAnimation(dataList.length);
+                      });
+                    }
+
+                    if (dataList.isEmpty) {
+                      return Expanded(child: PText(title: 'no_doctors_found'.tr()));
+                    } else {
+                      return Expanded(
+                        child: ListView.builder(
+                          itemCount: dataList.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            final item = dataList[index];
+                            final visible = _visibleItems.length > index && _visibleItems[index];
+
+                            return AnimatedOpacity(
+                              opacity: visible ? 1.0 : 0.0,
+                              duration: const Duration(milliseconds: 500),
+                              child: AnimatedSlide(
+                                duration: const Duration(milliseconds: 500),
+                                offset: visible ? Offset.zero : const Offset(0, 0.2),
+                                child: FavouriteItemWidget(
+                                  onCardClick: () {
+                                    context.push(AppRouter.doctorProfileScreen, extra: {
+                                      'id': item.doctor?.id ?? 0,
+                                      'name': item.doctor?.name ?? '',
+                                      'specialization': item.doctor?.specialization ?? '',
+                                    });
+                                  },
+                                  onTap: () {
+                                    context.push(AppRouter.appointmentBookingScreen, extra: {
+                                      'id': item.doctor?.id ?? 0,
+                                      'name': item.doctor?.name ?? '',
+                                      'specialization': item.doctor?.specialization ?? '',
+                                    });
+                                  },
+                                  imageUrl: item.doctor?.photo ?? '',
+                                  doctorName: item.doctor?.name ?? '',
+                                  rating: 4,
+                                  location: '',
+                                  specialization: item.doctor?.specialization ?? '',
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  },
+                  loadingWidget: Expanded(child: Center(child: CustomLoader(size: 35))),
+                  emptyWidget: (state) {
+                    return Expanded(
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.hourglass_empty),
+                            PText(title: 'no_doctors_found'.tr(), size: PSize.text18),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ],
-            )));
-          },)
-        ],),
-      ),),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
